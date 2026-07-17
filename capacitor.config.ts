b9639e3460ad9f183de4d3a2b016076e969ed286
cang-dot <WEISTUDIOWEI@126.com>
2026-07-17 10:17:31 +0800

    fix: 修正 capacitor.config.ts webDir 路径为 out/renderer

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.zephyrus.player',
   appName: 'ZephyrusPlayer',
-  webDir: 'dist/renderer',
+  webDir: 'out/renderer',
   server: {
     androidScheme: 'https'
   },
